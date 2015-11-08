f9ff3b84d2e0b7b5602a185233e03d2295318e5c
Aleksey Leontiev <aleksey.leontiev@icloud.com>
2015-11-08 11:12:15 +0300

    save content

--- modules/editor/editor.js
+++ modules/editor/editor.js
@@ -3,6 +3,7 @@ function init(app) {
   subscribe([
     { id: "key.down",             handler: onKeyDown },
     { id: "brain.thought.open",   handler: onBrainThoughtOpen },
+    { id: "brain.thought.close",   handler: onBrainThoughtClose },
     { id: "brain.thought.select", handler: onBrainThoughtSelect },
   ])
 }
@@ -13,14 +14,30 @@ function onKeyDown(event) {
   if (event.char == "O" && event.ctrlKey) {
     notify("brain.thought.open", activeThought)
   }
+
+  if (event.which == 27) {
+    notify("brain.thought.close", activeThought)
+  }
 }
 
 function onBrainThoughtOpen(thought) {
-  $("#details").toggle()
+  $("#details").show()
+  CKEDITOR.instances.editor1.setData(activeThought.content || "")
+}
+
+function onBrainThoughtClose(thought) {
+  save()
+  $("#details").hide()
+  activeThought = null
 }
 
 function onBrainThoughtSelect(thought) {
   activeThought = thought
+}
+
+function save() {
+  activeThought.content = CKEDITOR.instances.editor1.getData()
+  notify("brain.thought.changed", activeThought)
 }
 
 var activeThought = null
