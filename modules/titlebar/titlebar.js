c3be28f2cc5ae1c4887444c36ff52d07f774c103
Aleksey Leontiev <aleksey.leontiev@icloud.com>
2015-11-15 23:20:22 -0800

    titlebar: opacity on hover

--- modules/titlebar/titlebar.js
+++ modules/titlebar/titlebar.js
@@ -6,18 +6,6 @@ function init(app, config) {
   appendView(config.moduleRootPath + "view.html", "body")
 
   titlebar = $("#left-titlebar")
-
-  subscribe([
-    { id: "mouse.move", handler: onMouseMove }
-  ])
-}
-
-function onMouseMove(event) {
-  if (event.pageX < 32) {
-    titlebar.css("opacity", 1)
-  } else {
-    titlebar.css("opacity", 0)
-  }
 }
 
 var titlebar = null
